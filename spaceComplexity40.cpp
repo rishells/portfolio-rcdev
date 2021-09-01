0642381a3022c994811957f5d712471e0aaf98c6
Ricardo Conchas <ricardoconchas@gmail.com>
2021-09-01 10:04:19 -0500

    add Ex2

--- spaceComplexity40.cpp
+++ spaceComplexity40.cpp
@@ -4,14 +4,31 @@ using namespace std;
 
 
 
-int sum(int n){
+int sum(int n){ /*Ex 1*/
     if(n <=0){
         return 0;
     }
     return n + sum(n-1);
 }
+
+int pairSum(int a, int b){
+    return a + b;
+}
+
+int pairSumSequence(int n){ /* Ex 2*/
+    int sum = 0;
+    for (int i = 0; i < n; i ++){
+        sum += pairSum(i, i + 1);
+    }
+
+    return sum;
+}
+
+
+
+
 int main()
 {
-    cout<<sum(4)<<endl;
+    cout<<pairSumSequence(5)<<endl;
     return 0;
 }
